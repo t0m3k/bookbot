1f4875969ab27fb1ea4aadf493365a439ad66afa
Tomasz Tracz <t@t90.dev>
2024-05-25 18:19:16 +0100

    feat: Add initial project files and configuration
    This commit adds the initial project files and configuration. It includes the following changes:
    - Added .gitignore file to exclude the 'books' directory from version control
    - Added .vscode/launch.json file for VS Code configuration
    - Added main.py file with initial implementation of the 'main' function and helper functions for generating a report on the 'books/frankenstein.txt' file

---
+++ main.py
@@ -0,0 +1,47 @@
+def main():
+    with open("books/frankenstein.txt") as file:
+        text = file.read()
+        letters = letter_stats(text)
+        sorted = sort_dict(letters)
+
+        print("--- Begin report of books/frankenstein.txt ---")
+        print()
+        print(f"{number_of_words(text)} words found in the document")
+        for stat in sorted:
+            print(
+                f"Character '{stat["letter"]}' appeared {stat["value"]} times."
+            )
+        print()
+        print("--- End report ---")
+
+
+def number_of_words(text):
+    words = text.split()
+    return len(words)
+
+
+def letter_stats(text: str):
+    stats = dict()
+    for letter in text.lower():
+        if letter == " " or letter == "\n":
+            continue
+        curr = stats.get(letter, 0)
+        stats.update({letter: curr + 1})
+    return stats
+
+
+def sort_on(dict):
+    return dict["value"]
+
+
+def sort_dict(dict: dict):
+    array = []
+    for letter in dict.keys():
+        array.append({"letter": letter, "value": dict[letter]})
+
+    array.sort(reverse=True, key=sort_on)
+
+    return array
+
+
+main()
